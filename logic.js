e8f67d4fa9cd19ca0aa2878f6722d6ecb88a8436
Antonio <tony.formisano.2000@gmail.com>
2026-03-24 12:46:40 +0100

    creazione grafico a torta

--- logic.js
+++ logic.js
@@ -1,18 +1,25 @@
-const results = (numeroRisposteCorrette) => {
-  const percentualeRisposteCorrette = (numeroRisposteCorrette / 10) * 100;
+const FACSIMILEVOTO = 7;
+const domanda = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const results = () => {
+  const percentualeRisposteCorrette = (FACSIMILEVOTO / domanda.length) * 100;
   console.log(percentualeRisposteCorrette + "%");
-  const nEserciziCorretti = numeroRisposteCorrette + "in 10 questions";
+  const nEserciziCorretti = FACSIMILEVOTO + ` / ${domanda.length} questions`;
   console.log(nEserciziCorretti);
 
   const percentualeRisposteSbagliate = 100 - percentualeRisposteCorrette;
   console.log(percentualeRisposteSbagliate + "% ");
 
-  const nEserciziSbagliati = 10 - numeroRisposteCorrette + "in 10 questions";
+  const nEserciziSbagliati =
+    domanda.length - FACSIMILEVOTO + ` / ${domanda.length} questions`;
   console.log(nEserciziSbagliati);
 
-  const tornte = document.getElementsByClassName("torte");
-  tornte.setAttribute("width", "100px");
-  tornte.setAttribute("height", "100px");
-  tornte.setAttribute("boarderRadius", "50%");
-  tornte.setAttribute("backgroundColor", "black");
+  const torte = document.getElementsByClassName("Torte")[0];
+  torte.style.width = "300px";
+  torte.style.height = "300px";
+  torte.style.borderRadius = "50%";
+  torte.style.background = `conic-gradient(#00ffff 0% ${percentualeRisposteSbagliate}% , #970082 ${percentualeRisposteCorrette}% 100%)`;
+  const maskRule = "radial-gradient(circle, transparent 55%, black 56%)";
+  torte.style.WebkitMaskImage = maskRule;
+  torte.style.maskImage = maskRule;
 };
+results(6);
