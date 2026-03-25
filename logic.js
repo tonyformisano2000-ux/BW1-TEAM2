37b92c902c21bd7d6984a218c0fdb3f8a358d132
Antonio <tony.formisano.2000@gmail.com>
2026-03-25 10:25:22 +0100

    si

--- logic.js
+++ logic.js
@@ -1,43 +1,5 @@
 // LOGICA PAGE 2
 // timer
-
-results(7);
-const results = (voto) => {
-  const percentualeRisposteCorrette = (voto / domanda.length) * 100;
-  console.log(percentualeRisposteCorrette + "%");
-  const nEserciziCorretti = voto + ` / ${domanda.length} questions`;
-  console.log(nEserciziCorretti);
-
-  const percentualeRisposteSbagliate = 100 - percentualeRisposteCorrette;
-  console.log(percentualeRisposteSbagliate + "% ");
-
-  const nEserciziSbagliati =
-    domanda.length - voto + ` / ${domanda.length} questions`;
-  console.log(nEserciziSbagliati);
-
-  const torte = document.getElementsByClassName("Torte")[0];
-  torte.style.width = "250px";
-  torte.style.height = "250px";
-  torte.style.borderRadius = "50%";
-  torte.style.background = `conic-gradient(#d20094 0% ${percentualeRisposteSbagliate}%, #00ffff ${percentualeRisposteSbagliate}% 100% )`;
-  const maskRule = "radial-gradient(circle, transparent 47%, black 47.1%)";
-  torte.style.WebkitMaskImage = maskRule;
-  torte.style.maskImage = maskRule;
-  torte.style.boxShadow = "0 0 10px 10px gold"; // TOCCA CAPIRE PERCHE NON METTE LE SHADOWS
-  let innerCircleTextH6 = document.querySelector(".Torte>h6");
-  let innerCircleTextP = document.querySelector(".Torte>p");
-  if (voto > 6) {
-    innerCircleTextH6.innerHTML =
-      "Congratulations!<span><b>You passed the exam</b></span>";
-    innerCircleTextP.innerText =
-      "We'll send you the certificate in few minutes. check your email (including promotions/spam folder)";
-  } else {
-    innerCircleTextH6.innerHTML =
-      "Oops! <span><b>You failed the exam</b></span>";
-    innerCircleTextP.innerText = "come back soon and try again, good luck!";
-  }
-};
-
 let contatore = null;
 let nrDomanda = document.querySelectorAll("footer p span");
 
@@ -161,4 +123,4 @@ const mostraDomande = function (arr) {
 };
 mostraDomande();
 localStorage.setItem(`punteggio`, `${punteggio}`);
-window.location.href = "./page3/page3.html";
+// window.location.href = "./page3/page3.html";
