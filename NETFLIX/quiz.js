70f2bb6a0ac2a1a9c98f327fe218c3a658529bfb
Antonio <tony.formisano.2000@gmail.com>
2026-03-25 15:42:54 +0100

    creation netflix

---
+++ NETFLIX/quiz.js
@@ -0,0 +1,127 @@
+// LOGICA PAGE 2
+// timer
+let contatore = null;
+let nrDomanda = document.querySelectorAll("footer p span");
+
+function avviaTimer() {
+  clearInterval(contatore);
+  let secondi = 30;
+  document.getElementById("numero").textContent = secondi;
+  document.getElementById("arco").style.strokeDashoffset = 283;
+  contatore = setInterval(function () {
+    secondi = secondi - 1;
+    document.getElementById("numero").textContent = Math.ceil(secondi);
+    let nascosto = (283 * secondi) / 30;
+    document.getElementById("arco").style.strokeDashoffset = nascosto;
+
+    if (secondi <= 0) {
+      clearInterval(contatore);
+      indice++;
+      nrDomanda[0].innerText = indice + 1;
+      mostraDomande();
+    }
+  }, 1000);
+}
+// fine timer
+//SCALA STELLINE
+const stars = document.querySelectorAll("#stars span");
+let savedIndex = -1;
+
+stars.forEach((star, indice) => {
+  star.addEventListener("click", () => {
+    savedIndex = indice;
+    stars.forEach((s, i) => s.classList.toggle("clicked", i <= savedIndex));
+  });
+
+  star.addEventListener("mouseenter", () => {
+    star.classList.add("hovered");
+    stars.forEach((s, i) => s.classList.toggle("clicked", i <= indice));
+  });
+
+  star.addEventListener("mouseleave", () => {
+    star.classList.remove("hovered");
+    stars.forEach((s, i) => s.classList.toggle("clicked", i <= savedIndex));
+  });
+});
+
+// js per la parte delle stelle
+// inizio quiz
+const domande = [
+  {
+    domanda: "Chi ha vinto il Mondiale 2006?",
+    risposte: ["Francia", "Italia", "Germania", "Brasile"],
+    corretta: 1,
+  },
+
+  {
+    domanda: "Chi è il miglior marcatore della storia della Champions League?",
+    risposte: ["Messi", "Cristiano Ronaldo", "Lewandowski", "Benzema"],
+    corretta: 1,
+  },
+  {
+    domanda: "In quale squadra ha giocato Diego Armando Maradona in Italia?",
+    risposte: ["Juventus", "Roma", "Napoli", "Inter"],
+    corretta: 2,
+  },
+  {
+    domanda: "Quanti giocatori ci sono in campo per squadra?",
+    risposte: ["9", "10", "11", "12"],
+    corretta: 2,
+  },
+  {
+    domanda: "Quale nazionale ha vinto più Mondiali?",
+    risposte: ["Italia", "Germania", "Argentina", "Brasile"],
+    corretta: 3,
+  },
+];
+let indice = 0;
+let punteggio = 0;
+
+const domanda = document.querySelector(".quiz-layout h2");
+const bottoni = document.querySelectorAll(".answer-btn");
+avviaTimer();
+
+const mostraDomande = function (arr) {
+  // SE FINISCONO LE DOMANDE
+  if (indice >= domande.length) {
+    domanda.innerText = `Quiz finito!! Hai totalizzato ${punteggio} punti su ${domande.length}`;
+    localStorage.setItem("punteggio", punteggio);
+    const nextPageBtn = document.createElement("a");
+    nextPageBtn.innerText += "See the result";
+    nextPageBtn.classList.add("answer-btn", "next-page-btn");
+    nextPageBtn.setAttribute("href", "../page3/page3.html");
+
+    domanda.appendChild(nextPageBtn);
+    bottoni.forEach((bottone) => (bottone.style.display = "none"));
+    document.querySelector(".timer").style.display = "none";
+    document.querySelector("footer p").style.visibility = "hidden";
+  }
+
+  //RESETTIAMO IL TIMER
+  avviaTimer();
+
+  // QUANTE DOMANDE ABBIAMO?
+  nrDomanda[0].innerText = `QUESTION  ${indice + 1}`;
+  nrDomanda[1].innerHTML = `/ <span style=" color: #d20094;">${domande.length}</span>`;
+
+  //MOSTRIAMO SEMPRE LA DOMANDA CORRISPONDENTE
+  domanda.innerText = domande[indice].domanda;
+
+  //MOSTRIAMO LE RISPOSTE CORRISPONDENTI
+  bottoni.forEach((bottone, i) => {
+    bottone.innerText = domande[indice].risposte[i];
+
+    //AL CLICK FAI QUESTO
+    bottone.onclick = function () {
+      if (i === domande[indice].corretta) {
+        punteggio++;
+      }
+      indice++;
+
+      nrDomanda[0].innerText = `QUESTION  ${indice + 1}`;
+
+      mostraDomande();
+    };
+  });
+};
+mostraDomande();
